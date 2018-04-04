7e75cc0ba0b6817a4716e7da3d5de237adfdc8d3
Nick Nicholas <opoudjis@optusnet.com.au>
2018-04-05 09:45:09 +1000

    update docs

--- rice.sh
+++ rice.sh
@@ -1,1 +1,3 @@
 asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.adoc
+asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.sector.adoc
+asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.local.adoc
