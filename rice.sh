b56a6a67679defe628cdb6f4bace3e6d410c27e8
Ronald Tse <ronald.tse@ribose.com>
2018-04-06 12:20:06 +0800

    Consolidate common body-[zh,en]

--- rice.sh
+++ rice.sh
@@ -1,3 +1,13 @@
+
+echo ">>> Generating GB Standard documents ..."
 bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.zh.adoc
-#asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.sector.adoc
-#asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.local.adoc
+bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.en.adoc
+
+echo ">>> Generating Sector Standard documents ..."
+bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.sector.zh.adoc
+bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.sector.zh.adoc
+
+echo ">>> Generating Local Standard documents ..."
+bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.local.zh.adoc
+bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.local.zh.adoc
+
