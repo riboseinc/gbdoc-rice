1d77c4a72bb05dce56311f94aa57235e5ce6216d
Ronald Tse <ronald.tse@ribose.com>
2018-04-06 12:00:48 +0800

    Provide Chinese version of GB/T Rice document

--- rice.sh
+++ rice.sh
@@ -1,3 +1,3 @@
-asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.adoc
-asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.sector.adoc
-asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.local.adoc
+bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.zh.adoc
+#asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.sector.adoc
+#asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.local.adoc
