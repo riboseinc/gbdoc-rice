9ab888131ce6b821c0193cc1b1eaf8e4c28d5aa6
Nick Nicholas <opoudjis@optusnet.com.au>
2018-04-09 11:28:24 +1000

    rice.sh

--- rice.sh
+++ rice.sh
@@ -5,9 +5,9 @@ bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.en.adoc
 
 echo ">>> Generating Sector Standard documents ..."
 bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.sector.zh.adoc
-bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.sector.zh.adoc
+bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.sector.en.adoc
 
 echo ">>> Generating Local Standard documents ..."
 bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.local.zh.adoc
-bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.local.zh.adoc
+bundle exec asciidoctor --trace -b gb -r 'asciidoctor-gb' rice.gb.local.en.adoc
 
